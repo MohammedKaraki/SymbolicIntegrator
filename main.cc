77c1f6e477ce9bd2d4c5c732ab317e4028b2ea77
Mohammed Karaki <mhd.krki@gmail.com>
2020-07-08 00:33:24 -0400

    evict integrand function from main()

--- main.cc
+++ main.cc
@@ -13,18 +13,21 @@ double deriv(const F& func, double x)
   return (func(x+dx) - func(x-dx)) / (2.0*dx);
 }
 
+// The function to be integrated
+double func(double x)
+{
+  return std::cos(x)-std::tan(x);
+}
+
 int main()
 {
   std::vector<double> xs = {0.2, 0.5, 0.9, 1.5, 2.0, 3.0};
-  // auto func = [](double x) { return std::sin(x) / (1+std::sin(x)); };
-  // auto func = [](double x) { return x/((1+x)*(1+x)); };
-  auto func = [](double x) { return 1/(x*x-1); };
 
   std::mt19937 rng(15);
   std::uniform_int_distribution<int> uniform;
 
-  fprintf(stderr, "started\n");
-  for (int attempt = 0; ; ++attempt) {
+  fprintf(stderr, "Search started\n");
+  for (int attempt = 1; ; ++attempt) {
 
     int len = uniform(rng)%20 + 2;
     auto expr = symb::GenExpr(len);
@@ -46,7 +49,7 @@ int main()
     }
 
     if (attempt%500'000 == 0) {
-      fprintf(stderr, "%d\n", attempt);
+      fprintf(stderr, "%10dk attemps\n", attempt/1000);
     }
   }
 }
