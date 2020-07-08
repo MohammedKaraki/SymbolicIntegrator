91a29e2a1f7486191e7dfdd7f850740e73bec3d7
Mohammed Karaki <mhd.krki@gmail.com>
2020-07-07 22:56:25 -0400

    Split into .h and .cc

---
+++ symb.h
@@ -0,0 +1,22 @@
+#include <vector>
+#include <string>
+
+namespace symb {
+  // Each character in an expression corresponds to a function pointer of this
+  // type.
+  typedef void(*FuncPtr)();
+
+  // Generate random expression of a given length. Example: "ab+x/".
+  std::string GenExpr(int len);
+
+  // Compile an expression. That is, convert a vector of symbols
+  // into a vector of the corresponding function addresses.
+  std::vector<FuncPtr> Compile(const std::string& expr);
+
+  // Run a compiled expression and return the resulting number.
+  double Run(const std::vector<FuncPtr>& compiled_expr);
+  double Run(const std::vector<FuncPtr>& compiled_expr, double x);
+  double Run(const std::vector<FuncPtr>& compiled_expr, double x, double y);
+  double Run(const std::vector<FuncPtr>& compiled_expr, double x, double y,
+             double z);
+}
